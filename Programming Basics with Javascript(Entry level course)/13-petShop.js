10d4eafeb68539eb98d684a785a65669cb4f5d94
Dariyoig <darioignatov72@gmail.com>
2021-12-08 15:07:24 +0200

    Create 13-petShop.js

---
+++ Programming Basics with Javascript(Entry level course)/13-petShop.js
@@ -0,0 +1,7 @@
+function calculateFoodCost([dogBags, catBags]) {
+    let dogFoodCost = dogBags * 2.50;
+    let catFoodCost = catBags * 4;
+
+    console.log(`${dogFoodCost+ catFoodCost} lv.`);
+}
+calculateFoodCost(['4', '6']);

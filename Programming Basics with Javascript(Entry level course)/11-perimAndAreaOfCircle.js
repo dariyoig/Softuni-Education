e69fc08dcbbca371fbb700a4fbf2e13494a20a59
Dariyoig <darioignatov72@gmail.com>
2021-12-08 14:29:10 +0200

    exercise added

---
+++ Programming Basics with Javascript(Entry level course)/11-perimAndAreaOfCircle.js
@@ -0,0 +1,8 @@
+function solve(param) {
+    let area = Math.PI * param * param;
+    let perimeter = 2 * Math.PI * param;
+
+    console.log(`Area = ${area}`);
+    console.log(`Perimeter = ${perimeter}`);
+}
+solve(3);

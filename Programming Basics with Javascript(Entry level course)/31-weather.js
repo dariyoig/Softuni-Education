39075d9d8e7122583ddc4c4986de3ffd67f7f8b0
Dariyoig <darioignatov72@gmail.com>
2021-12-09 13:27:06 +0200

    Create 31-weather.js

---
+++ Programming Basics with Javascript(Entry level course)/31-weather.js
@@ -0,0 +1,8 @@
+function printCondition([adjective]) {
+     
+    if (adjective === 'sunny') {
+        console.log("It's warm outside!");
+    }else {
+        console.log("It's cold outside!");
+    }
+}

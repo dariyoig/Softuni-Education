bdc42a91d8648e03ff7feccb1b389ab45cafc347
Dariyoig <darioignatov72@gmail.com>
2021-12-17 18:08:58 +0200

    Create 34-biggerNumber.js

---
+++ Programming Basics with Javascript(Entry level course)/34-biggerNumber.js
@@ -0,0 +1,12 @@
+function solve([arg1, arg2]) {
+    
+    let firstNumber = Number(arg1);
+    let secondNumber = Number(arg2);
+
+    if (firstNumber > secondNumber)
+    console.log(firstNumber);
+    else
+    console.log(secondNumber);
+}
+
+solve (['3', '6'])

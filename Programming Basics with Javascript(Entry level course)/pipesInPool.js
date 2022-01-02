cd8def230af6a413a1d73a974c901fb3b21fb74a
Dariyoig <darioignatov72@gmail.com>
2022-01-02 13:23:18 +0200

    Update pipesInPool.js

--- Programming Basics with Javascript(Entry level course)/pipesInPool.js
+++ Programming Basics with Javascript(Entry level course)/pipesInPool.js
@@ -4,9 +4,24 @@ function solve([arg1, arg2, arg3, arg4]) {
     let secondPipeDebit = Number(arg3);
     let hoursGone = Number(arg4);
 
-    
-    console.log(`The pool is {запълненост на басейна в проценти}% full. Pipe 1: {процент вода от първата тръба}%. Pipe 2: {процент вода от втората тръба}%.`);
-    console.log(`For {часовете, които тръбите са пълнили вода} hours the pool overflows with {литрите вода в повече} liters.`);
+    let firstPipeFill = firstPipeDebit * hoursGone;
+    let secondPipeFill = secondPipeDebit * hoursGone;
+
+    let firstPipePercent;
+    let secondPipePercent;
+    let filled = firstPipeFill + secondPipeFill;
+    let percentFilled;
+    let overflow = filled - poolVolume;
+    if (poolVolume >= filled) {
+        percentFilled = filled / poolVolume * 100;
+        firstPipePercent = firstPipeFill / filled * 100;
+        secondPipePercent = secondPipeFill / filled * 100;
+        console.log(`The pool is ${percentFilled.toFixed(2)}% full. Pipe 1: ${firstPipePercent.toFixed(2)}%. Pipe 2: ${secondPipePercent.toFixed(2)}%.`);
+
+    } else {
+        console.log(`For ${hoursGone} hours the pool overflows with ${(overflow).toFixed(2)} liters.`);
+    }
+
 
 
 }
